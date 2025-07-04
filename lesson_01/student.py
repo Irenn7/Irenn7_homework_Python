13940b61c8f5c8b877bf3bd291967f31b9d1e20e
Your Name <you@example.com>
2025-07-04 22:15:38 +0300

    lesson_01

---
+++ lesson_01/student.py
@@ -0,0 +1,11 @@
+def print_letter (let):
+    print(let, end='')
+
+    print_letter('S')
+    print_letter('t')
+    print_letter('u')
+    print_letter('d')
+    print_letter('e')
+    print_letter('n')
+    print_letter('t')
+    
