d0c8f9ad391f12749987290e86f938d339270792
Your Name <you@example.com>
2025-07-06 12:42:12 +0300

    lesson_1_task_5.py

--- lesson_01/lesson_1_task_5.py
+++ lesson_01/lesson_1_task_5.py
@@ -1,6 +1,17 @@
-def print_number(num):
-    print(num)
+def num (let):
+    print(let, end= '')
+          
+          
+num (8)
+num (8)
+num (0)
+num (0)
+num (5)
+num (5)
+num (5)
+num (3)
+num (5)
+num (3)
+num (5)
 
 
-for digit in str (88005553535):
-    print_number(digit)
