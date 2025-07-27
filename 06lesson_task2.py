fe40e343ed513a5fb4886130939e17c5ce4d9798
Irenn7 <gsr667556@gmail.com>
2025-07-27 11:58:02 +0300

    Add files via upload

--- 06lesson_task2.py
+++ 06lesson_task2.py
@@ -3,23 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Используем Chrome в качестве браузера (не забудьте установить chromedriver!)
 driver = webdriver.Chrome()
-
-    # Перейти на нужную страницу
 driver.get("http://uitestingplayground.com/textinput")
 
 element = WebDriverWait (driver, 10).until(
-    EC.presence_of_element_located((By.ID, '#text'))
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'updatingButton'))
     )
 
-input_field = driver.find_element(By.ID, "newButtonName")
-input_field.send_keys("SkyPro")
+input = driver.find_element(By.ID, "updatingButton")
+input.send_keys("SkyPro")
 
-submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-submit_button.click()
+click = driver.find_element(By.CSS_SELECTOR, "updatingButton").click()
+text = driver.find_element(By.CSS_SELECTOR, "updatingButton").text
 
-updated_button_text = driver.find_element(By.XPATH, "//button[@id='updatingButton']").text
-print(updated_button_text)
+print(text)
 
 driver.quit()
+
--- 06lesson_task3.py
+++ 06lesson_task3.py
@@ -1,30 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeServise
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-# Используем хром как браузерный движок
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager().install()))
 wait = WebDriverWait(driver, 30)
+
     # Переход на страницу
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
     # Ожидаем загрузки всей страницы и изображений
-element = wait.until(EC.text_to_be_present_in_element((By.ID, "Loading images"), "Done!"))
-images = wait.until(
-        EC.text_to_be_present_in_element((By.ID, "avard"))
-    )
+element = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#Loading images"), "Done!"))
 
+img_element = driver.find_element(By.CSS_SELECTOR, "#https://bonigarcia.dev/selenium-webdriver-java/img/award.png")
+    
     # Берём третью картинку (учитывая индексацию с нуля)
-third_image = images[2]
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#award")))
 
-    # Получаем значение атрибута src
-image_src = third_image.get_attribute("src")
+third_img_src = img_element[2]
 
-    # Выводим результат в консоль
-print(image_src)
+print(driver.find_element(By.CSS_SELECTOR, "#award").get_dom_attribute("src"))
+
+#удаляем 
+driver.execute_script("arguments[0].removeAttribute('src');", third_img_src)
 
     # Завершаем сессию браузера
 driver.quit()
+
+
+
