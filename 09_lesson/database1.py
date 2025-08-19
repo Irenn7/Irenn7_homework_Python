86b0180237b3d78d07c4b4109bbbb3c99da27bed
Your Name <you@example.com>
2025-08-19 14:48:51 +0300

    lesson9

--- 09_lesson/database1.py
+++ 09_lesson/database1.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-db_connection_string = 'postgresql://postgres:mypassword@localhost:5432/postgres'
+db_connection_string = 'postgresql://postgres:RoyaL@localhost:5432/postgres'
 db = create_engine(db_connection_string)
 engine = db
 
